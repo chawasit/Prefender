9d9f697289cc6b084bc8387c64bd6175b9013b7d
kawewut <kawewut.chu@gmail.com>
2016-11-11 19:02:18 +0700

    เพิ่ม meteoroid play state

---
+++ core/src/th/ac/cmu/eng/cpe/cpe200/Manager/MeteoroidManager.java
@@ -0,0 +1,7 @@
+package th.ac.cmu.eng.cpe.cpe200.Manager;
+
+/**
+ * Created by kawewut on 11/11/2016 AD.
+ */
+public class MeteoroidManager {
+}
---
+++ core/src/th/ac/cmu/eng/cpe/cpe200/states/PlayState.java
@@ -0,0 +1,7 @@
+package th.ac.cmu.eng.cpe.cpe200.states;
+
+/**
+ * Created by kawewut on 11/10/2016 AD.
+ */
+public class PlayState {
+}
